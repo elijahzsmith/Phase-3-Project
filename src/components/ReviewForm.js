a70edea94aecf88e2a7eba653d6c4a6b994f8508
Elijah Smith <elijahsmith.m@gmail.com>
2022-05-11 13:27:22 -0500

    post requests

--- src/components/ReviewForm.js
+++ src/components/ReviewForm.js
@@ -1,7 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 
 function ReviewForm() {
-  return <div>ReviewForm</div>;
+  const [starInput, setStarInput] = useState("");
+  const [reviewInput, setReviewInput] = useState("");
+  return (
+    <form>
+      Leave a review!
+      <input type="integer" value={starInput} placeholder="Star Rating" />
+      <textarea type="textarea" value={reviewInput} placeholder="Review..." />
+      <button>Submit</button>
+    </form>
+  );
 }
 
 export default ReviewForm;
