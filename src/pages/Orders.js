3c293f59c9c366e28188548b24ae50e1f61e8231
Elijah Smith <elijahsmith.m@gmail.com>
2022-05-12 19:11:11 -0500

    search and dropdown backend

--- src/pages/Orders.js
+++ src/pages/Orders.js
@@ -1,11 +1,17 @@
 import React from "react";
 import OrderItem from "../components/OrderItem";
 import Button from "@mui/material/Button";
+import Search from "../components/Search";
 
-function Orders({ inOrders, setInOrders, renderMenuItemsOrderPage }) {
-  console.log(renderMenuItemsOrderPage);
-function Orders({ inOrders, setInOrders, renderMenuItems }) {
-  // console.log(renderMenuItems);
+function Orders({
+  inOrders,
+  setInOrders,
+  renderMenuItemsOrderPage,
+  currSelection,
+  setCurrSelection,
+  currSearch,
+  setCurrSearch,
+}) {
   const renderOrderItems = inOrders.map((order) => {
     console.log(order);
     return (
@@ -22,18 +28,27 @@ function Orders({ inOrders, setInOrders, renderMenuItems }) {
     <div>
       <div className="order">
         <div className="orderLeft">
-          <div className="restaurantInfo">Sinatra's</div>
+          <div className="restaurantInfo">Sinatra's Finest</div>
           <div className="menuOrderItems">
-            this is where menu items go
+            <Search currSearch={currSearch} setCurrSearch={setCurrSearch} />
+            <select
+              className="food-filter"
+              onChange={(e) => setCurrSelection(e.target.value)}
+            >
+              {/* Not cuisine but I cant think of the word */}
+              <option value="choose">Choose your cuisine</option>
+              <option value="lunch">Lunch</option>
+              <option value="dinner">Dinner</option>
+              <option value="appetizer">Appetizer</option>
+              <option value="dessert">Dessert</option>
+              <option value="drinks">Drinks</option>
+            </select>
             {renderMenuItemsOrderPage}
           </div>
         </div>
         <div className="orderRight">
           Your Order
-          <div className="menuOrderItems">
-            this is where order items go
-            {renderOrderItems}
-          </div>
+          <div className="menuOrderItems">{renderOrderItems}</div>
         </div>
         {/* <h1>Orders Page</h1> */}
         {/* {renderOrderItems} */}
