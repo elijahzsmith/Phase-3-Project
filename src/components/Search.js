f0f46fe904642c828405cd6c1847c0f51b501d4b
Elizabeth <elizabethtreahy@gmail.com>
2022-05-12 21:33:12 -0400

    Seach Bar on Order Form Design

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,17 +1,42 @@
 import React from "react";
-
+import TextField from '@mui/material/TextField'
+import {styled} from '@mui/material/styles';
 function Search({ currSearch, setCurrSearch, afterSelection,
   currSelection,
   setCurrSelection }) {
+    // const CssTextField = styled(TextField)({
+    //   '& label.Mui-focused': {
+    //     color: 'pink',
+    //   },
+    //   '& .MuiInput-underline:after': {
+    //     borderBottomColor: 'pink',
+    //     color: 'pink',
+    //   },
+    //   '& .MuiOutlinedInput-root': {
+    //     '& fieldset': {
+    //       borderColor: 'pink',
+    //       color: 'pink',
+    //     },
+    //     '&:hover fieldset': {
+    //       borderColor: 'pink',
+    //       color: 'pink',
+    //     },
+    //     '&.Mui-focused fieldset': {
+    //       borderColor: 'pink',
+    //       color: 'pink',
+    //     },
+    //   },
+    // });
   return (
     <div>
-      {/* Make controlled form */}
       <form>
+      {/* <CssTextField placeholder='what are you craving?' style={{ color: 'pink' }} id="custom-css-outlined-input" /> */}
         <input
           type="text"
           value={currSearch}
           onChange={(e) => setCurrSearch(e.target.value)}
-          placeholder="Search menu..."
+          placeholder="What are you craving?"
+          style={{background: 'black', color: 'pink'}}
         ></input>
       </form>
       {/* Insert image of food/restaurant */}
